24a0ed78532661eefced0ac648317c32e9f82e2d
mastero <castro.alejandro17@gmail.com>
2023-01-26 15:16:53 -0600

    new elements on profile

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { getDatabase, ref, update, child, get, onValue } from 'firebase/database';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  imageUrls = [
+    '', '', '', '', '', '', '', '', '', '','','', '', '', '', '', '', '',
+  ]; // Este arreglo contiene las URLs de las imágenes (18x)
+  hasMoreImages = true; // Esta bandera indica si hay más imágenes para cargar
+  trabajo = {
+             profesion: 'Service',
+             };
+  nombre: any;
+  profesion: any;
+  review: any;
 
-  constructor() { }
+  constructor() {
+    this.nombre = this.getNombre();
+    this.profesion = this.getProfesiones();
+    this.review = 'Name';
+   }
 
   ngOnInit() {
+  }
+
+  getNombre() {
+    const db = getDatabase();
+    const starCountRef = ref(db, '/id/nombres/' + '/nombre');
+      onValue(starCountRef, (snapshot) => {
+    this.nombre = snapshot.val();
+    });
+  }
+
+  getProfesiones() {
+    const db = getDatabase();
+    const work = ref(db, '/id/profesiones/' + '/profesion');
+      onValue(work, (snapshot) => {
+    this.profesion = snapshot.val();
+    });
   }
 
 }
