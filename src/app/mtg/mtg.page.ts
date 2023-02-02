6e0ceac24d2c595975d665bd421b0bb548461fde
mastero <castro.alejandro17@gmail.com>
2023-02-01 20:00:11 -0600

    Add Axios for HttpRequest Add new elements for Axios Request to APi

--- src/app/mtg/mtg.page.ts
+++ src/app/mtg/mtg.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
 
-/*import { pokemon } from './../../../node_modules/pokemontcgsdk';*/
+import axios from 'axios';
 
 @Component({
   selector: 'app-mtg',
@@ -13,18 +14,44 @@ export class MtgPage implements OnInit {
   imageUrls = [''];
   build = {
     message: 'Under Construction',
-    what: 'Pokemon TGC'
+    what: 'Pokemon TGC',
   };
+  card: any;
+  imgeUrls = [];
+  element: any;
+  name: any;
+  stage: any;
+  types: any;
+  hp: any;
+  priceTCGPlayer: any;
 
-  constructor() {
-    /*pokemon.configure({apiKey: ''});*/
+  constructor(private http: HttpClient) {
+    /*PokemonTCG.({ apiKey: '402a62a4-e172-4e5e-bf0c-f8d0ebc83541' });*/
   }
 
   ngOnInit() {}
 
-  /*getPokemon() {
-    pokemon.card.find('base1-4').then((card) => {
-      console.log(card.name); // "Charizard"
-    });
-  }*/
+  getPokemon() {
+    PokemonTCG.findCardByID('xy7-54').then((card: PokemonTCG.Card) => {
+      console.log(card.name);// Gardevoir
+  });
+  }
+
+  getAxiosData(){
+    axios.get('https://api.pokemontcg.io/v2/cards/xy7-52')
+    .then(response => this.element = response.data);
+    console.log(this.element.data.images.large);
+    this.imgeUrls = this.element.data.images.large;
+    this.name = this.element.data.name;
+    this.stage = this.element.data.subtypes;
+    this.types = this.element.data.types;
+    this.hp = this.element.data.hp;
+    this.priceTCGPlayer = this.element.data.tcgplayer.prices.normal.market;
+  }
+
+  getAxiosData2(){
+    const element = document.querySelector('#get-request .result');
+     axios.get('https://api.npms.io/v2/search?q=axios')
+      .then(response => element.innerHTML = response.data.total);
+  }
 }
